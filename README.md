42db3035b582fa7da3f04ac770845c3fbbe9139a
Vu <vu.chau@chartboost.com>
2023-01-10 21:56:59 -0500

    1.1.0

--- README.md
+++ README.md
@@ -1,27 +1,57 @@
 # reddit-markdown
 
 ## Introduction
-This Ruby script saves the content of a Reddit post into a Markdown file for easy reading, sharing, and archiving.
+This Ruby script saves Reddit posts into local Markdown files for easy reading, sharing, and archiving. Both post body and replies are supported.
+
+<table>
+	<tr>
+		<td><img src="demo/demo_markdown.jpg" /></td>
+		<td><img src="demo/demo_markdown_rendered.jpg" /></td>
+	</tr>
+</table>
 
 ## Usage
-1. **Install Ruby on your computer**
+1. **Install Ruby on your device**
     * https://www.ruby-lang.org/en/documentation/installation/
-2. **Download the script and save it to a folder**
+2. **Download the [latest release](https://github.com/chauduyphanvu/reddit-markdown/releases) of this script**
 3. **Open a terminal**
 4. **Run the script with the following command:**
     * `ruby reddit-markdown.rb`
     * If you call the script from a different folder, you need to specify the path to the script
         * `ruby /path/to/reddit-markdown.rb`
     * Tip: You can rename the script to anything and place it anywhere you want
-5. **Enter the link to the Reddit post you want to save**
-6. **Enter the path where you want to save the Markdown file**. 
+5. **Enter the link(s) to the Reddit post(s) you want to save**
+6. **Enter the path where you want to save the Markdown file(s)**.
     * Leave blank to save in the same folder (where you called the script from)
+	* Tip: Starting with the 1.1.0 release, you can set a default path in the `settings.json` file. See [Custom Settings](#custom-settings) for details.
+
+## Custom Settings
+Starting with the 1.1.0 release, a number of settings can be customized. They can be found in the `settings.json` file bundled with the script. 
+
+If you are using an older release, make sure to get the latest version of the script (plus the `settings.json` file) to tweak these settings. On the other hand, if you are on the latest release but somehow don't have the `settings.json` file, you can get it directly from this repository. 
+
+**Note**: _Settings exposed in `settings.json` are locked to a specific version of the script. If you are using different editions of the two files, you may get unexpected results. It is recommended to always use the latest version of both files._
+
+| Setting Flag | Description | Possible values |
+| --- | --- | --- |
+| "version" | The version of the script that the settings are compatible with. Do NOT change. | Semantically versioned string |
+| "update_check_on_startup" | Whether to check for updates on startup | true/false |
+| "show_upvotes" | Whether to render the number of upvotes | true/false |
+| "reply_depth_color_indicators" | Whether to render color indicators for reply depths | true/false |
+| "line_break_between_parent_replies" | Whether to render a line break between parent replies | true/false |
+| "show_auto_mod_comment" | Whether to render AutoModerator's comment | true/false |
+| "overwrite_existing_file" | Whether to overwrite existing file if the file name already exists. If set to `false`, a number (starting with 1) will be appended to the file name. | true/false |
+| "save_posts_by_subreddits" | Whether to separate saved files into subfolders named after their subreddits. If set to `true`, the subreddit name will be the name of the subfolder. If set to `false`, files will be saved together under `default_save_location` or a user-provided path. | true/false |
+| "default_save_location" | The default path to save the Markdown file(s). If `save_posts_by_subreddits` is set to `true`, the subreddit name will be appended to the default path. | Path string set as an environment variable <sup>1</sup> |
+
+<sub>1. _The path string must be set as an environment variable. The key name in `settings.json` and for your environment variable must be `DEFAULT_REDDIT_SAVE_LOCATION`. See [Use environment variables in Terminal on Mac](https://support.apple.com/guide/terminal/use-environment-variables-apd382cc5fa-4f58-4449-b20a-41c53c006f8f/mac), [Create and Modify Environment Variables on Windows](https://docs.oracle.com/en/database/oracle/machine-learning/oml4r/1.5.1/oread/creating-and-modifying-environment-variables-on-windows.html#GUID-DD6F9982-60D5-48F6-8270-A27EC53807D0), or [How to Set Environment Variables in Linux](https://www.serverlab.ca/tutorials/linux/administration-linux/how-to-set-environment-variables-in-linux/) for more details._</sub>
 
 ## Advantages
 * **Open source, and free**
 * **No authentication/sign-in of any kind needed**
 	* Publicly available data. Safe and no security concerns
 * **Look and feel similar to browsing Reddit on desktop web**
+	* Bonus: Plenty of customizations available
 * **Supports both text and multimedia posts**
 * **Runs anywhere Ruby runs**
 	* Ruby is cross-platform
@@ -29,9 +59,6 @@ This Ruby script saves the content of a Reddit post into a Markdown file for eas
 * **Markdown for universality**
 
 ## Limitations
-* **Only 1 post at a time**
-	* Each time you run this script, you are saving only content from 1 reddit.com link.
-	* However, it's trivial to write your own script that calls this script consecutively with a set of links.
 * **Only replies that are visible by default on the web are saved**
 	* Reddit hides a subset of replies based on upvotes that you can manually click to show. Those won't be saved by this script.
 * **Only supported on desktop**
--- reddit_markdown.rb
+++ reddit_markdown.rb
@@ -5,6 +5,41 @@ require 'uri'
 
 puts "This script saves the content (body and replies) of a Reddit post to a Markdown file for easy reading, sharing, and archiving."
 
+if !File.exist?("settings.json")
+    puts "Error: settings.json not found. Please get a copy of it from https://github.com/chauduyphanvu/reddit-markdown/releases. Exiting..."
+    exit
+end
+
+settings = JSON.parse(File.read("settings.json"))
+
+version = settings['version']
+update_check_on_startup = settings['update_check_on_startup']
+show_auto_mod_comment = settings['show_auto_mod_comment']
+line_break_enabled = settings['line_break_between_parent_replies']
+show_upvotes_enabled = settings['show_upvotes']
+reply_depth_color_indicators_enabled = settings['reply_depth_color_indicators']
+overwrite_existing_file_enabled = settings['overwrite_existing_file']
+save_posts_by_subreddits = settings['save_posts_by_subreddits']
+directory = settings["default_save_location"]
+
+if update_check_on_startup == true
+    commits = JSON.parse(URI.open("https://api.github.com/repos/chauduyphanvu/reddit-markdown/releases").read)
+
+    if commits.length == 0
+        puts "Warning: Unable to fetch latest release info from GitHub. Please check for updates manually. The repo might have been renamed/deleted."
+    end
+    
+    latest_version = commits.first["tag_name"]
+        
+    if latest_version.match?(/\d+\.\d+\.\d+/)
+        if Gem::Version.new(latest_version) > Gem::Version.new(version)
+            puts "\nSuggestion: A new version (#{latest_version}) is available. Your current version is #{version}. You can download the latest version from https://github.com/chauduyphanvu/reddit-markdown."
+        end
+    else
+        puts "\nWarning: Found invalid version number in latest GitHub commit. Please check for updates manually at https://github.com/chauduyphanvu/reddit-markdown."
+    end    
+end
+
 # Supported colors to differentiate between replies of different depths.
 colors = [
     "🟩",
@@ -13,8 +48,7 @@ colors = [
     "🟦",
     "🟪",
     "🟥",
-    "🟫",
-    "🟤",
+    "🟫",    
     "⬛️",
     "⬜️"
 ]
@@ -24,23 +58,46 @@ puts "\n"
 # Example of a "clean" Reddit link
 # This script also supports links that have other query parameters appended (that happens when you use the "Share" button to get the link)
 # https://www.reddit.com/r/pcmasterrace/comments/101kjyq/my_dad_has_been_playing_civilization_almost_daily/
+puts "=> Enter the link to the Reddit post that you want to save. Separate multiple links with commas."
+urls = gets.chomp
 
-puts "Enter the link to the Reddit post that you want to save:"
-url = gets.chomp
+while urls == nil || urls == ""
+    puts "Error: No links provided. Try again."
+    urls = gets.chomp
 
-# URLs that are shared from Reddit may have query parameters appended.
-# Drop them to get a clean URL.
-if url.include? "?utm_source"
-    url = url.split("?utm_source").first
+    puts "\n"
 end
 
-if url == nil || url == ""
-    puts "Error: URL is empty. Try again."
-    exit
-end
- 
 puts "\n"
-puts "Downloading post data..."
+
+# To avoid having to enter the save location every time, you can set the DEFAULT_REDDIT_SAVE_LOCATION environment variable.
+# For it to take effect, the env var must be set once BEFORE running the script, and 
+# the default_save_location value in settings.json must be set to "DEFAULT_REDDIT_SAVE_LOCATION".
+if directory == "DEFAULT_REDDIT_SAVE_LOCATION"
+    directory = ENV["DEFAULT_REDDIT_SAVE_LOCATION"]
+
+    if directory == nil || directory == ""
+        puts "Error: DEFAULT_REDDIT_SAVE_LOCATION environment variable not set. You must set it to a valid path before running the script. 
+        If you'd rather be prompted for the save location every time, set the default_save_location value in settings.json to \"\"."
+        puts "Exiting..."
+        exit
+    end
+else
+    puts "=> Enter a full path to save the post(s) to. Hit Enter/Return for current directory, which is #{Dir.pwd}."
+    directory = gets.chomp
+    directory = directory.strip
+
+    if directory == ""
+        directory = Dir.pwd
+    end
+        
+    while (!File.directory?(directory)) 
+        puts "Error: Invalid path. Try again."
+        directory = gets.chomp
+    
+        puts "\n"
+    end     
+end
 
 # By appending ".json" to the end of a Reddit post URL, we can get the JSON payload for the post.
 # This way we don't have to actually tap into the Reddit API. No authentication is required.
@@ -57,23 +114,6 @@ def download_post_json(url)
         json
     }
 end
-
-# Use the last part of the URL (after the last slash) for the file name.
-file_name = url.split("/").last
-
-# The entire JSON payload
-json = download_post_json(url)
-
-if json == nil || json == ""
-    puts "Error: JSON payload is empty. Try again."
-    exit
-end
-
-# The post body and relevant metadata
-post_info = json[0]['data']['children']
-
-# The replies
-response = json[1]['data']['children']
 
 # Get all the child replies to a parent (top-level) reply.
 def get_replies(reply)
@@ -104,102 +144,173 @@ def get_replies(reply)
     child_replies
 end
 
-content = "**#{post_info[0]['data']['subreddit_name_prefixed']}** | Posted by u/#{post_info[0]['data']['author']}\n\n"
-content += "## #{post_info[0]['data']['title']}\n\n"
-content += "Original post: [#{post_info[0]['data']['url']}](#{post_info[0]['data']['url']})\n\n"
+# Resolve the file name based on a number of rules.
+def resolve_full_path(url, directory, overwrite_existing_file_enabled, save_posts_by_subreddits, subreddit)
+    file_name = url.split("/").last
+    subreddit = subreddit.gsub("r/", "")
+    full_path = directory
 
-# The post body as text, if any
-post_text = "#{post_info[0]['data']['selftext'].gsub(/\n/, "\n> ")}"
+    if save_posts_by_subreddits == true
+        full_path = "#{directory}/#{subreddit}"
 
-# The post body as a media, if any
-# This will get the first one if there's only one. 
-# We currently don't support multiple medias in a single post (post body and replies will still be downloaded but medias will be ignored).
-post_media = post_info[0]['data']['url_overridden_by_dest']
+        if !File.directory?(full_path)
+            FileUtils.mkdir_p(full_path)
+        end
+    end
+            
+    # If we have to use a timestamp, no need to worry about duplicate file names.
+    if file_name == nil || file_name == ""
+        puts "Error: Could not get file name from URL. Using current timestamp as file name..."
+        return "#{full_path}/reddit_no_name_#{Time.now.to_i}"
+    end
 
-if post_media != nil && post_media != ""
-    content += "![#{post_info[0]['data']['title']}](#{post_media})\n\n"
+    full_path = "#{full_path}/#{file_name}"
+    copy_num = 0
+
+    if (File.exist?("#{full_path}.md"))
+        if overwrite_existing_file_enabled == true
+            puts "File with name #{file_name}.md already exists. Overwriting is enabled. Overwriting..."
+            return "#{full_path}.md"
+        end
+    
+        copy_num += 1
+
+        while File.exist?("#{full_path}_#{copy_num}.md")
+            copy_num += 1
+        end    
+    end
+
+    if copy_num > 0
+        puts "File with name #{file_name}.md already exists. Overwriting is disabled. Renaming to #{file_name}_#{copy_num}.md...\n"
+        full_path = "#{full_path}_#{copy_num}"
+    end
+
+    "#{full_path}.md"
 end
 
-if post_text != nil && post_text != ""
-    content += "> #{post_text}\n\n"
-end
+urls = urls.split(",")
+urls.each_with_index do |url, index|
+    puts "#{index + 1}. #{url}"
 
-content += "---\n\n"
+    # URLs that are shared from Reddit may have query parameters appended.
+    # Drop them to get a clean URL.
+    if url.include? "?utm_source"
+        url = url.split("?utm_source").first
+    end
 
-response[0...response.length].each do |reply|
-    content += "* #{colors[0]} **#{reply['data']['author']}** ⬆️ #{reply['data']['ups']} ⬇️ #{reply['data']['downs']}  \n"
-
-    # Parent (1st-level) reply, from which we'll get all the child replies.
-    reply_body = reply['data']['body']
-
-    # On the web, Reddit hides a subset of child replies that you'd have to manually click to see.
-    # Those child replies typically have very low upvotes and are usually just spam.
-    # This script preserves that experience and skips child replies that fall into that category.
-    if reply_body == nil || reply_body == ""
+    if url == nil || url == ""
+        puts "Error: Invalid URL. Skipping..."
         next
     end
 
-    # Format the reply body such that each new line is properly indented.
-    # Also if the reply body contains `&gt;`, replace it with `>` so quotes properly show up.
-    reply_formatted = reply_body.gsub(/\n/, "\n\t")
-    reply_formatted = reply_formatted.gsub(/&gt;/, ">")
-    content += "\t#{reply_formatted}\n\n"
+    puts "\n"
+    puts "Downloading post data..."
 
-    child_replies = get_replies(reply)
-    child_replies.each do |id, child_reply|
-        content += "\t" * child_reply['depth']
-        content += "* #{colors[child_reply['depth']]} **#{child_reply['child_reply']['data']['author']}** ⬆️ #{child_reply['child_reply']['data']['ups']} ⬇️ #{child_reply['child_reply']['data']['downs']}  \n"
+    # The entire JSON payload
+    json = download_post_json(url)
 
-        # Have a different indentation for child reply depending on its depth.
-        tabs = "\t"
-        child_reply['depth'].times do |i| 
-            tabs += "\t"
+    if json == nil || json == ""
+        puts "Error: JSON payload for #{url} is empty. Skipping..."
+        next
+    end
+
+    # The post body and relevant metadata
+    post_info = json[0]['data']['children']
+
+    # The replies
+    response = json[1]['data']['children']
+
+    op = post_info[0]['data']['author']
+    subreddit = post_info[0]['data']['subreddit_name_prefixed']
+    content = "**#{subreddit}** | Posted by u/#{op}\n\n"
+    content += "## #{post_info[0]['data']['title']}\n\n"
+    content += "Original post: [#{post_info[0]['data']['url']}](#{post_info[0]['data']['url']})\n\n"
+
+    # The post body as text, if any
+    post_text = "#{post_info[0]['data']['selftext'].gsub(/\n/, "\n> ")}"
+
+    # The post body as a media, if any
+    # This will get the first one if there's only one. 
+    # We currently don't support multiple medias in a single post (post body and replies will still be downloaded but medias will be ignored).
+    post_media = post_info[0]['data']['url_overridden_by_dest']
+
+    if post_media != nil && post_media != ""
+        content += "![#{post_info[0]['data']['title']}](#{post_media})\n\n"
+    end
+
+    if post_text != nil && post_text != ""
+        content += "> #{post_text}\n\n"
+    end
+
+    content += "---\n\n"
+
+    response[0...response.length].each do |reply|            
+        author = reply['data']['author']
+
+        if author == "AutoModerator" && show_auto_mod_comment == false
+            next
         end
 
-        # Format the child reply body such that each *subsequent new line* is indented by the depth of the reply.
-        # Also if the child reply body contains `&gt;`, replace it with `>` so quotes properly show up.
-        child_reply_formatted = child_reply['child_reply']['data']['body'].gsub(/\n/, "\n#{tabs}")
-        child_reply_formatted = child_reply_formatted.gsub(/&gt;/, ">")
+        if author == op
+            author += " (OP)"
+        end
 
-        # The formatted child reply still needs to be indented by x number of tabs for the first line.
-        content += "#{tabs}#{child_reply_formatted}\n\n"
+        content += "* #{reply_depth_color_indicators_enabled ? colors[0] : ""} **#{author}** #{show_upvotes_enabled ? "⬆️ #{reply['data']['ups']}" : ""}\n\n"
+
+        # Parent (1st-level) reply, from which we'll get all the child replies.
+        reply_body = reply['data']['body']
+
+        # On the web, Reddit hides a subset of child replies that you'd have to manually click to see.
+        # Those child replies typically have very low upvotes and are usually just spam.
+        # This script preserves that experience and skips child replies that fall into that category.
+        if reply_body == nil || reply_body == ""
+            next
+        end
+
+        # Format the reply body such that each new line is properly indented.
+        # Also if the reply body contains `&gt;`, replace it with `>` so quotes properly show up.
+        reply_formatted = reply_body.gsub(/\n/, "\n\t")
+        reply_formatted = reply_formatted.gsub(/&gt;/, ">")
+        content += "\t#{reply_formatted}\n\n"
+
+        child_replies = get_replies(reply)
+        child_replies.each do |id, child_reply|
+            content += "\t" * child_reply['depth']
+            author = child_reply['child_reply']['data']['author']
+
+            if author == op
+                author += " (OP)"
+            end
+
+            content += "* #{reply_depth_color_indicators_enabled ? colors[child_reply['depth']] : ""} **#{author}** #{show_upvotes_enabled ? "⬆️ #{child_reply['child_reply']['data']['ups']}" : ""}\n\n"
+
+            # Have a different indentation for child reply depending on its depth.
+            tabs = "\t"
+            child_reply['depth'].times do |i| 
+                tabs += "\t"
+            end
+
+            # Format the child reply body such that each *subsequent new line* is indented by the depth of the reply.
+            # Also if the child reply body contains `&gt;`, replace it with `>` so quotes properly show up.
+            child_reply_formatted = child_reply['child_reply']['data']['body'].gsub(/\n/, "\n#{tabs}")
+            child_reply_formatted = child_reply_formatted.gsub(/&gt;/, ">")
+
+            # The formatted child reply still needs to be indented by x number of tabs for the first line.
+            content += "#{tabs}#{child_reply_formatted}\n\n"
+        end
+
+        if line_break_enabled == true
+            content += "---\n\n"
+        end
     end
+
+    content += "\n"
+    full_path = resolve_full_path(url, directory, overwrite_existing_file_enabled, save_posts_by_subreddits, subreddit)
+
+    puts "Saving to #{full_path}...\n"
+
+    File.open(full_path, "w") { |file| file.write(content) }
+
+    puts "Reddit post saved! Check it out at #{full_path}."
+    puts "\n---\n"
 end
-
-content += "\n"
-
-puts "\n"
-puts "Enter a path to save the file to. Leave blank for current directory (where you called the script from):"
-directory = gets.chomp
-
-if directory == ""
-    directory = Dir.pwd
-end
-
-if !Dir.exist?(directory)
-    puts "Invalid directory. Exiting."
-    exit
-end
-
-# If the file already exists, still save it but append a number to the file name.
-# There is value in having multiple copies of the same post saved, especially if the post/replies have been updated.
-copy_num = 0
-
-if (File.exist?("#{directory}/#{file_name}.md"))
-    copy_num += 1
-
-    while File.exist?("#{directory}/#{file_name}_#{copy_num}.md")
-        copy_num += 1
-    end    
-end
-
-if copy_num > 0
-    puts "File with name #{file_name}.md already exists. Renaming to #{file_name}_#{copy_num}.md...\n\n"
-    file_name = "#{file_name}_#{copy_num}"
-end
-
-puts "Saving to #{directory}/#{file_name}.md...\n\n"
-
-File.open("#{file_name}.md", "w") { |file| file.write(content) }
-
-puts "Reddit post saved! Check #{directory}/#{file_name}.md."
